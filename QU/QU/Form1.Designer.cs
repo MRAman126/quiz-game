57e40564a7dc0b909babcd9d3451e28ddd065260
MRAman126 <125187210+MRAman126@users.noreply.github.com>
2023-09-03 13:29:22 +0530

    Add files via upload

---
+++ QU/QU/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace QU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(16, 449);
+            this.lblQuestion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(939, 57);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.lblQuestion_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 510);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 510);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(137, 589);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(624, 589);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QU.Properties.Resources.start_img;
+            this.pictureBox1.Location = new System.Drawing.Point(4, -4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(939, 412);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(853, 654);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Tag = "5";
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(971, 705);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ QU/QU/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QU
+{
+    public partial class Form1 : Form
+    {
+        //quize game variables
+
+        int correctAnswer;
+       
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+     
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+            totalQuestions = 8;
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+
+            }
+           
+            if(questionNumber == totalQuestions)
+            {
+                // work out the percentage
+
+                percentage = (int)Math.Round((Double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Quize Ended!" + Environment.NewLine + 
+                    "You have Answered" + score + "Question Correctly Answered. "  +
+                    Environment.NewLine + "Your total Persentage is " + percentage + "%"
+                    + Environment.NewLine + "Click Ok to Play again"
+
+
+
+
+                    );
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+
+            }
+
+                questionNumber++;
+                askQuestion(questionNumber);
+
+
+
+
+            }
+
+
+
+        
+
+        private void askQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+
+                case 1:
+                    pictureBox1.Image = Properties.Resources.start_img;
+
+                    lblQuestion.Text = "Are you Excited to start the game ?";
+
+                    button1.Text = "Yes";
+                    button2.Text = "No";
+                    button3.Text = "Can't Wait";
+                    button4.Text = "Never";
+
+                    correctAnswer = 1;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.greenland_img;
+
+                    lblQuestion.Text = "Which is the largest island in the world?";
+
+                    button1.Text = "Iraland";
+                    button2.Text = "Andaman";
+                    button3.Text = "Green land";
+                    button4.Text = "Goa";
+                    button5.Text = " Back";
+                   
+
+                    correctAnswer = 3;
+                
+                   
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.sord_img;
+
+                    lblQuestion.Text = "What is the name of the sword owned by King Arthur?\r\n\r\n";
+
+                    button1.Text = "Zulfikar";
+                    button2.Text = "Excalibar";
+                    button3.Text = "Zubida";
+                    button4.Text = "Sord of Honor";
+                    button5.Text = " Back";
+                    
+                    correctAnswer = 2;
+             
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.geometry_img;
+
+                    lblQuestion.Text = "What is the name of the sword owned by King Arthur?\r\n\r\n";
+
+                    button1.Text = "Danial ";
+                    button2.Text = "Thor";
+                    button3.Text = "Ratan Sigh";
+                    button4.Text = "Euclid";
+                    button5.Text = " Back";
+                    
+
+                    correctAnswer = 4;
+          
+
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.india_flag;
+
+                    lblQuestion.Text = "Who wrote the national song of India?\r\n\r\n";
+
+                    button1.Text = "Galif ";
+                    button2.Text = "Neharu";
+                    button3.Text = "Rabindranath tagore";
+                    button4.Text = "Harish chandra";
+                    button5.Text = " Back";
+                   
+
+                    correctAnswer = 3;
+                
+
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.girl_child;
+
+                    lblQuestion.Text = "When is National Girl Child Day celebrated in India?\r\n\r\n";
+
+                    button1.Text = "Feb 24 ";
+                    button2.Text = "Jan 24";
+                    button3.Text = "Sep 24";
+                    button4.Text = "Agu 24";
+                    button5.Text = " Back";
+                   
+
+                    correctAnswer = 2;
+
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.leap_year;
+
+                    lblQuestion.Text = " A leap year has _____ days?\r\n\r\n";
+
+                    button1.Text = "361";
+                    button2.Text = "365";
+                    button3.Text = "367";
+                    button4.Text = "366";
+                    button5.Text = " Back";
+                   
+
+                    correctAnswer = 4;
+                 
+
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.india_flag;
+
+                    lblQuestion.Text = "Taj Mahal is situated on the banks of which river?\r\n\r\n";
+
+                    button1.Text = "Yamuna ";
+                    button2.Text = "Jmuna";
+                    button3.Text = "Bhagirathi";
+                    button4.Text = "Ganga";
+                    button5.Text = " Back";
+                
+
+                    correctAnswer = 1;
+                 
+
+                    break;
+                
+
+
+
+
+            }
+
+
+        }
+
+        
+    }
+}
